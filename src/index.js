465210cfa2c3ac5dd38503cf5f4db1a84e6bb122
Noor Fadhila Kamal <noor.fadhila05@gmail.com>
2022-06-22 04:45:30 +0700

    add footer

--- src/index.js
+++ src/index.js
@@ -1,15 +1,15 @@
 import React from 'react';
 import App from './app';
 import 'normalize.css';
-import { GlobalStyles } from './global-styles';
-import { createRoot } from 'react-dom/client';
+import {GlobalStyles} from './global-styles';
+import {createRoot} from 'react-dom/client';
 
 const container = document.getElementById('root');
 const root = createRoot(container);
 
 root.render(
-<>
-    <GlobalStyles />
-    <App />
-</>
+    <>
+        <GlobalStyles />
+        <App />
+    </>
 );
