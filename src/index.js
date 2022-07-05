8db7a95437d2398b59d61489bc317a0da8767d45
Noor Fadhila Kamal <noor.fadhila05@gmail.com>
2022-07-05 18:30:48 +0700

    seed data to Firebase (update code to Firebase ver 9.8.4 - July 2022)

--- src/index.js
+++ src/index.js
@@ -3,6 +3,7 @@ import App from './app';
 import 'normalize.css';
 import {GlobalStyles} from './global-styles';
 import {createRoot} from 'react-dom/client';
+import { firebase } from './lib/firebase.prod';
 
 const container = document.getElementById('root');
 const root = createRoot(container);
